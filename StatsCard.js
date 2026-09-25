02c2ac4aa7129b3bdce2dc69814d990d21712491
younas557761-arch <younas557761@gmail.com>
2026-09-25 18:26:44 +0500

    Delete StatsCard.js

--- StatsCard.js
+++
@@ -1,3 +0,0 @@
-export default function StatsCard({ label, value, icon }) {
-  return <div className="card p-5"><div className="text-sm text-slate-400">{icon} {label}</div><div className="mt-2 text-3xl font-bold">{value}</div></div>;
-}
