2352d99997bbbda60e3f47352138383ee18f8bae
younas557761-arch <younas557761@gmail.com>
2026-09-25 18:30:57 +0500

    Delete tools.js

--- tools.js
+++
@@ -1,7 +0,0 @@
-export function normalizeSearch(value = "") {
-  return value.trim().replace(/[%_]/g, "");
-}
-
-export function required(value) {
-  return typeof value === "string" && value.trim().length > 0;
-}
