3c818a47515d2c8fe9ef3651708b86c3015343f2
younas557761-arch <younas557761@gmail.com>
2026-09-25 18:29:44 +0500

    Delete layout.js

--- layout.js
+++
@@ -1,5 +0,0 @@
-import Sidebar from "@/components/Sidebar";
-import Header from "@/components/Header";
-export default function DashboardLayout({children}) {
- return <div className="flex min-h-screen bg-slate-900"><Sidebar/><div className="min-w-0 flex-1"><Header/><main className="p-4 md:p-8">{children}</main></div></div>;
-}
