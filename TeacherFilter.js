e9edd475f451edaa2899679480561951e38f7778
younas557761-arch <younas557761@gmail.com>
2026-09-25 18:27:40 +0500

    Delete TeacherFilter.js

--- TeacherFilter.js
+++
@@ -1,3 +0,0 @@
-export default function TeacherFilter({ value, onChange }) {
-  return <input value={value} onChange={e=>onChange(e.target.value)} placeholder="Filter specialization..." className="rounded-lg border border-slate-700 bg-slate-900 px-3 py-2"/>;
-}
