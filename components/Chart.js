70b08118879b6ef4362b4d312e4a0a53938e5b04
younas557761-arch <younas557761@gmail.com>
2026-09-26 11:11:45 +0500

    Create Chart.js

---
+++ components/Chart.js
@@ -0,0 +1,7 @@
+export default function Chart() {
+  return (
+    <div className="bg-[#1e293b] border border-[#334155] rounded-xl p-6 h-64 flex items-center justify-center text-slate-500">
+      Chart Component Placeholder (Recharts)
+    </div>
+  );
+}
